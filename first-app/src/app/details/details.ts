30448e911287b361086d53e2ed39b1f6f55ed152
Oliver Forral <intrepion@gmail.com>
2025-07-26 11:25:52 -0600

    Adding a form to your Angular app

--- first-app/src/app/details/details.ts
+++ first-app/src/app/details/details.ts
@@ -1,11 +1,12 @@
 import {Component, inject} from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {Housing} from '../housing';
 import {HousingLocationInfo} from '../housinglocation';
 
 @Component({
   selector: 'app-details',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './details.html',
   styleUrl: './details.scss'
 })
@@ -13,8 +14,20 @@ export class Details {
   route: ActivatedRoute = inject(ActivatedRoute);
   housing = inject(Housing);
   housingLocation: HousingLocationInfo | undefined;
+  applyForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+    email: new FormControl(''),
+  });
   constructor() {
     const housingLocationId = Number(this.route.snapshot.params['id']);
     this.housingLocation = this.housing.getHousingLocationById(housingLocationId);
+  }
+  submitApplication() {
+    this.housing.submitApplication(
+      this.applyForm.value.firstName ?? '',
+      this.applyForm.value.lastName ?? '',
+      this.applyForm.value.email ?? '',
+    );
   }
 }
