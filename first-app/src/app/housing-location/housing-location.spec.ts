163f85316c83cf0ffc16f002cfaa16692a801375
Oliver Forral <intrepion@gmail.com>
2025-07-26 00:43:24 -0600

    Add an interpolation to a component’s template

--- first-app/src/app/housing-location/housing-location.spec.ts
+++ first-app/src/app/housing-location/housing-location.spec.ts
@@ -1,19 +1,31 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { HousingLocation } from './housing-location';
+import { HousingLocationInfo } from '../housinglocation';
 
 describe('HousingLocation', () => {
   let component: HousingLocation;
   let fixture: ComponentFixture<HousingLocation>;
+  const mockHousingLocation: HousingLocationInfo = {
+    id: 99,
+    name: 'Test Home',
+    city: 'Test City',
+    state: 'TS',
+    photo: 'test.jpg',
+    availableUnits: 10,
+    wifi: true,
+    laundry: false,
+  };
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HousingLocation]
-    })
-    .compileComponents();
+      imports: [HousingLocation],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(HousingLocation);
     component = fixture.componentInstance;
+
+    fixture.componentRef.setInput('housingLocation', mockHousingLocation);
+
     fixture.detectChanges();
   });
 
