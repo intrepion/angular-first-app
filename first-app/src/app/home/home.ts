64ef2177a56580244353110d4fa74be4a798b76d
Oliver Forral <intrepion@gmail.com>
2025-07-26 11:51:53 -0600

    Add the search feature to your app

--- first-app/src/app/home/home.ts
+++ first-app/src/app/home/home.ts
@@ -5,25 +5,24 @@ import { HousingLocationInfo } from '../housinglocation';
 @Component({
   selector: 'app-home',
   imports: [HousingLocation],
-  template: `
-    <section>
-      <form>
-        <input type="text" placeholder="Filter by city" />
-        <button class="primary" type="button">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      @for(housingLocation of housingLocationList; track $index) {
-        <app-housing-location [housingLocation]="housingLocation"></app-housing-location>
-      }
-    </section>
-  `,
+  templateUrl: './home.html',
   styleUrls: ['./home.scss'],
 })
 export class Home {
   housingLocationList: HousingLocationInfo[] = [];
   housing: Housing = inject(Housing);
+  filteredLocationList: HousingLocationInfo[] = [];
   constructor() {
     this.housingLocationList = this.housing.getAllHousingLocations();
+    this.filteredLocationList = this.housingLocationList;
+  }
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+      return;
+    }
+    this.filteredLocationList = this.housingLocationList.filter((housingLocation) =>
+      housingLocation?.city.toLowerCase().includes(text.toLowerCase()),
+    );
   }
 }
