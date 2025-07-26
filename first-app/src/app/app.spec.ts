c726685854f527b2dde2375f026e25cadb7b8e21
Oliver Forral <intrepion@gmail.com>
2025-07-26 11:07:08 -0600

    Add routes to the application

--- first-app/src/app/app.spec.ts
+++ first-app/src/app/app.spec.ts
@@ -1,23 +1,31 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
 import { App } from './app';
+import routeConfig from './app.routes';
+import { Home } from './home/home';
+import { Details } from './details/details';
 
 describe('App', () => {
+  let fixture: ComponentFixture<App>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App],
+      imports: [App, Home, Details],
+      providers: [provideRouter(routeConfig)],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(App);
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(App);
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it('should render button', () => {
-    const fixture = TestBed.createComponent(App);
+  it('should render router outlet', async () => {
     fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('button')?.textContent).toContain('Search');
+    await fixture.whenStable();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('router-outlet')).toBeTruthy();
   });
 });
--- first-app/src/app/app.ts
+++ first-app/src/app/app.ts
@@ -1,10 +1,9 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {Home} from './home/home';
+import {RouterModule} from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [Home],
+  imports: [RouterModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
